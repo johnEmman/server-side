89faf214e57318489255191ac69673d36ecee966
john emman <emmanuel11052002@gmail.com>
2024-11-14 02:18:19 +0800

    upidate

--- src/signaling.ts
+++ src/signaling.ts
@@ -3,14 +3,11 @@ import { Server } from "socket.io";
 export const handleSignaling = (io: Server) => {
   let connectedUsers: string[] = []; // Track connected users
   const users: any = {};
-  io.on("connection", (socket) => {
-    let userId = socket.handshake.query.userId;
-    if (!userId) {
-      userId = `user-${Date.now()}`; // Generate a simple unique user ID
-    }
-    users[socket.id] = userId;
-    console.log("A user connected:", socket.id);
+  const rooms: Record<string, string[]> = {}; // Store room ID with user list
 
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+    let userId = socket.handshake.query.userId;
     // Add the new user to the list
     connectedUsers.push(socket.id);
 
@@ -19,42 +16,6 @@ export const handleSignaling = (io: Server) => {
 
     // Notify all clients about the new user
     io.emit("user-connected", socket.id);
-
-    // Handle creating a room and joining it
-    socket.on("create-room", (roomId: string) => {
-      console.log(`Room created with ID: ${roomId}`);
-
-      // Join the room
-      socket.join(roomId);
-
-      // Emit the room creation and notify all users in the room
-      io.to(roomId).emit("room-created", { roomId, creatorId: socket.id });
-      console.log(`${socket.id} joined room ${roomId}`);
-    });
-    socket.on("join-room", (roomId) => {
-      socket.join(roomId); // Join the room
-      io.to(roomId).emit("user-joined-room", socket.id); // Notify others in the room
-    });
-
-    socket.on("leave-room", (roomId) => {
-      socket.leave(roomId); // Leave the room
-      io.to(roomId).emit("user-left-room", socket.id); // Notify others in the room
-    });
-
-    // Handle incoming messages and broadcast them to the target user
-    socket.on(
-      "sendMessage",
-      (
-        messageData: { senderId: string; message: string },
-        targetId: string
-      ) => {
-        console.log(
-          `Message sent from ${messageData.senderId} to ${targetId}: ${messageData.message}`
-        );
-        // Emit the message to the target user
-        io.to(targetId).emit("message", messageData);
-      }
-    );
 
     socket.on("disconnect", () => {
       console.log("A user disconnected:", socket.id);
